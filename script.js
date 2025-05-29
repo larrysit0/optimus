339c77880445639697ccdec010a34e0b63349161
larrysit0 <63930869+larrysit0@users.noreply.github.com>
2025-05-28 23:49:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("coffee-cup").addEventListener("click", function() {
+    const popup = document.getElementById("coffee-popup");
+    popup.classList.toggle("hidden");
+});
